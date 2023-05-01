c4c3122500becc9cd94b10946c5abeedca402c1a
Braigthon <fambal66@gmail.com>
2023-04-30 18:44:31 -0600

    final
    me duele los ojos...

--- hex.js
+++ hex.js
@@ -1,5 +1,4 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-// #f15025
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
@@ -13,6 +12,6 @@ btn.addEventListener("click", function () {
   document.body.style.backgroundColor = hexColor;
 });
 
- function getRandomNumber(){
- return Math.floor (Math.random() * hex.length);
+function getRandomNumber(){
+  return Math.floor(Math.random() * hex.length);
 }
